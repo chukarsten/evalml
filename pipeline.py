a36e83925427968bc7c6a9f94b14b25fe200a3e2
unknown <karstenchu@gmail.com>
2020-11-10 23:34:39 -0500

    Solid unit test for init().  Finished Pipeline:predict() and Pipeline:metrics().  Need to work on some of the test cases for the pipeline and different data types/shapes.

--- pipeline.py
+++ pipeline.py
@@ -1,41 +1,74 @@
-from evalml.pipelines.components import (Imputer, LogisticRegressionClassifier,
-                                         OneHotEncoder)
+import copy
+from evalml.pipelines import DecisionTreeClassifier
+from evalml.pipelines.components.transformers import Transformer
+from evalml.pipelines.components.estimators import Estimator
+from evalml.objectives import F1, Recall, Precision, AUC, AccuracyBinary, AccuracyMulticlass
+from evalml.model_understanding import confusion_matrix
 
-class DummyPipeline:
+class KeystoneXL:
     """Your code here!"""
 
     def __init__(self, components, parameters, random_state):
-        self.components = components
-        self.parameters = parameters
+        self.parameters = copy.deepcopy(parameters)
         self.random_state = random_state
+        self.components = []
 
         for component in components:
-            if isinstance(component(), Imputer):
-                print("IMP:", component)
-                self.imputer = component(**self.parameters["Imputer"])
-            if isinstance(component(), OneHotEncoder):
-                print("OHE:", component)
-                self.one_hot_encoder = component(**self.parameters["One Hot Encoder"])
-            if isinstance(component(), LogisticRegressionClassifier):
-                print("LRC:", component)
-                self.log_reg_clf = component(**self.parameters["Logistic Regression Classifier"])
+            if isinstance(component(), Estimator):
+                self.parameters[component.name]["random_state"] = self.random_state
+            self.components.append(component(**self.parameters[component.name]))
 
     def fit(self, X_train, y_train):
-        if self.imputer:
-            imputer = self.imputer
-            imputer.fit(X_train, y_train)
-            imputer_out = imputer.transform(X_train)
-        if self.one_hot_encoder:
-            ohe = self.one_hot_encoder
-            ohe.fit(imputer_out, y_train)
-            ohe_out = ohe.transform(imputer_out, y_train)
-        if self.log_reg_clf:
-            lrc = self.log_reg_clf
-            lrc.fit(ohe_out, y_train)
-            return lrc.predict(ohe_out)
+        out = X_train
+        for component in self.components:
+            component.fit(out, y_train)
+            if isinstance(component, Transformer):
+                out = component.transform(out)
+            if isinstance(component, Estimator):
+                return component.predict(out)
+
+    def predict(self, X_test):
+        """Function to run X_test through pipeline and evaluate predictions
+        against requrested metrics.
+
+        Arguments:
+            X_test (np.ndarray): array of the test input data to predict on
+            y_test (np.ndarray): array of the test output data to score against
+        Returns:
+            y_pred (np.ndarray):
+
+        """
+
+        out = X_test
+        for component in self.components:
+            if isinstance(component, Transformer):
+                out = component.transform(out)
+            if isinstance(component, Estimator):
+                out = component.predict(out)
+        return out
+
+    def metrics(self, y_predicted, y_true, metrics):
+        """Function to run requested metrics on outputs.
+
+        Arguments:
+            y_predicted (np.ndarray): array of the predicted labels
+            y_true (np.ndarray): array of the truth labels
+            metrics (list of evalml.objectives): list of metrics to evaluate against
+        Returns:
+            results (dict): dictionary of requested metrics scores
+
+        """
+        results = {}
+        for metric in metrics:
+            metric_str = metric.__name__
+            metric_score = metric().score(y_predicted, y_true)
+            print("%s: %f" % (metric_str, metric_score))
+            results[metric_str] = metric_score
+        return results
 
 
-    def predict(self):
-        pass
+
+
+
 
 
--- unit_tests.py
+++ unit_tests.py
@@ -1,9 +1,13 @@
+import numpy as np
 import pytest
-from evalml.pipelines.components import (Imputer, LogisticRegressionClassifier,
-                                         OneHotEncoder)
+from evalml.objectives import F1, Recall, Precision, AUC, AccuracyBinary, AccuracyMulticlass, R2, MSE
+from evalml.pipelines.components import Imputer, LogisticRegressionClassifier, OneHotEncoder
+from evalml.pipelines.components.estimators import Estimator
+from evalml.pipelines.components.transformers import Transformer
+from evalml.preprocessing import split_data
 from sklearn import datasets
 
-from pipeline import DummyPipeline
+from pipeline import KeystoneXL
 
 
 @pytest.fixture
@@ -14,9 +18,8 @@ def X_y_binary():
     return X, y
 
 
-def test_pipeline_creation(X_y_binary):
-    X, y = X_y_binary
-
+@pytest.fixture
+def get_test_params():
     parameters = {
         'Imputer': {
             "categorical_impute_strategy": "most_frequent",
@@ -31,9 +34,70 @@ def test_pipeline_creation(X_y_binary):
             'C': 1.0,
         }
     }
+    random_state = 42
     components = [Imputer, OneHotEncoder, LogisticRegressionClassifier]  # noqa: F841
-    pipeline = DummyPipeline(parameters=parameters,  # noqa: F841
-                             components=components,
-                             random_state=42)
-    import pdb; pdb.set_trace()
-    pipeline.fit(X,y)
+    metrics = [F1, AUC, Recall, Precision, AUC, AccuracyBinary, AccuracyMulticlass, R2, MSE]  # noqa: F841
+    return parameters, components, metrics, random_state
+
+
+def test_pipeline_creation(X_y_binary, get_test_params):
+    parameters, components, metrics, random_state = get_test_params
+    pipeline = KeystoneXL(parameters=parameters,  # noqa: F841
+                          components=components,
+                          random_state=random_state)
+
+    # Make sure all components are initialized and with proper parameters
+    for idx, p_comp in enumerate(pipeline.components):
+        assert isinstance(p_comp,
+                          components[idx]), "Pipeline: components are not initialized in same order as presented."
+        if isinstance(p_comp, Transformer):
+            assert p_comp.parameters.items() >= parameters[
+                p_comp.name].items(), "Pipeline: component parameters not set correctly."
+        elif isinstance(p_comp, Estimator):
+            correct_params = parameters[p_comp.name]
+            set_params = p_comp.parameters
+            set_params.pop('random_state', None)
+            assert set_params.items() >= correct_params.items(), "Pipeline: component parameters not set correctly."
+
+    # Make sure the random_state made it into the estimator
+    assert pipeline.random_state == random_state
+    assert pipeline.components[-1]._component_obj.get_params()["random_state"] == random_state
+
+
+def test_pipeline_fit(X_y_binary, get_test_params):
+    parameters, components, metrics, random_state = get_test_params
+    X, y = X_y_binary
+    X_train, X_test, y_train, y_test = split_data(X, y, random_state=random_state)
+    pipeline = KeystoneXL(parameters=parameters,  # noqa: F841
+                          components=components,
+                          random_state=random_state)
+    pipeline.fit(X_train, y_train)
+    y_pred = pipeline.predict(X_test)
+    np.testing.assert_allclose(y_pred.tolist(), [1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1])
+
+
+def test_pipeline_predict(X_y_binary, get_test_params):
+    X, y = X_y_binary
+    X_train, X_test, y_train, y_test = split_data(X, y)
+    parameters, components, metrics, random_state = get_test_params
+    pipeline = KeystoneXL(parameters=parameters,  # noqa: F841
+                          components=components,
+                          random_state=random_state)
+    pipeline.fit(X_train, y_train)
+    y_pred = pipeline.predict(X_test)
+    print(sum(y_pred.to_numpy() == y_test.to_numpy()) / len(y_pred))
+
+
+def test_pipeline_metrics(X_y_binary, get_test_params):
+    X, y = X_y_binary
+    X_train, X_test, y_train, y_test = split_data(X, y)
+    parameters, components, metrics, random_state = get_test_params
+    pipeline = KeystoneXL(parameters=parameters,  # noqa: F841
+                          components=components,
+                          random_state=random_state)
+    pipeline.fit(X_train, y_train)
+    y_pred = pipeline.predict(X_test)
+    print(sum(y_pred.to_numpy() == y_test.to_numpy()) / len(y_pred))
+
+# split_data() docs should have :return: param more explicit about the order of the
+# returns
